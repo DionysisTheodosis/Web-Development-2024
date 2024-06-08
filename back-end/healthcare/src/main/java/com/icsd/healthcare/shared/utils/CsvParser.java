93e1fe6e0f377a4750800cb55e4ce6be4774563b
Dionysis Theodosis <theo_187@hotmail.com>
2024-06-08 15:31:26 +0300

    After ExcelParser and CsvParser

--- back-end/healthcare/src/main/java/com/icsd/healthcare/shared/utils/CsvParser.java
+++ back-end/healthcare/src/main/java/com/icsd/healthcare/shared/utils/CsvParser.java
@@ -5,6 +5,7 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.bean.HeaderColumnNameMappingStrategy;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
@@ -15,9 +16,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Component
 public class CsvParser {
 
-    public static <T> Set<T> parseCsv(MultipartFile file, Class<T> clazz) throws IOException {
+    public  <T> Set<T> parseCsv(MultipartFile file, Class<T> clazz) throws IOException {
         try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
             HeaderColumnNameMappingStrategy<T> strategy = new HeaderColumnNameMappingStrategy<>();
             strategy.setType(clazz);
