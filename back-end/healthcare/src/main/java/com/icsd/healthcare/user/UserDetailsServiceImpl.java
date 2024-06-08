2fa25039e85d473d66f8c424a08420e65fa69122
Dionysis Theodosis <theo_187@hotmail.com>
2024-06-08 20:18:04 +0300

    working UserDetailsServiceImpl

--- back-end/healthcare/src/main/java/com/icsd/healthcare/user/UserDetailsServiceImpl.java
+++ back-end/healthcare/src/main/java/com/icsd/healthcare/user/UserDetailsServiceImpl.java
@@ -2,21 +2,33 @@ package com.icsd.healthcare.user;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
+import java.util.Collections;
+
+
 @Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email).
-                orElseThrow(() -> new UsernameNotFoundException("Email not found!"));
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(
+                () -> new UsernameNotFoundException("Username not found")
+        );
+        String role = user.getUserRole().name(); // Assume user.getRole() returns a Role object with a getName() method
+        GrantedAuthority authority = new SimpleGrantedAuthority(role);
+        return  new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.singletonList(authority)
+        );
     }
-
 }
